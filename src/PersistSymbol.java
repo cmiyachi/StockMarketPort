2193ebd2f10963b1e8945994f18e7aa34a530f9d
Chris  Miyachi <chrisyogini@gmail.com>
2014-02-05 16:24:38 -0500

    first interface defined with team

---
+++ src/PersistSymbol.java
@@ -0,0 +1,8 @@
+/**
+ * Created by cmiyachi on 2/5/14.
+ */
+public interface PersistSymbol {
+    void SaveSymbol(String symbol);
+    String  GetSymbols();
+
+}
---
+++ src/ScrapStockData.java
@@ -0,0 +1,6 @@
+/**
+ * Created by cmiyachi on 2/5/14.
+ */
+public interface ScrapStockData {
+    String GetStockData(String symbol);
+}
