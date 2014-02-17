9491e4f06f228b404c0e895b07f91fd1647ac339
zyanguml <i990802b@yahoo.com>
2014-02-16 19:36:23 -0500

    Added comments to describe methods.

--- src/persistence/PersistSymbol.java
+++ src/persistence/PersistSymbol.java
@@ -7,55 +7,77 @@ import java.util.Set;
 
 
 /**
- * Created by cmiyachi on 2/5/14.
+ * This is an interface for the class project.
+
+ * The methods that are to be implemented follow CRUD (create, read, update, delete).
+ * 
+ * Based on team discussions, Stock data is represented in the form of Strings.
+ * 
+ *
  */
 public interface PersistSymbol {
 
     /**
-     * Save the symbol
+     * Save and persist the Stock data based on symbol name.
      * @param symbol
      */
     public void saveSymbol(String symbol);
 
+    
+    
     /**
-     * Get list of symbols;
+     * Get Set of Symbol names.
      * @return
      */
     public Set<String>  getSymbols();
 
+    
     /**
-     * Read from the database;
+     * Read from dborfile (the database or file) to retrieve the Stock symbols and stock data.
      * @param dborfile
      */
     public void readFromDB(String dborfile);
 
+    
+    
+    
     /**
-     * Read a symbol.
+     * Read a Stock symbol from the database. 
+     * Note that a Stock symbol data is represented as hash table.
      * @param symbol
      * @return
      */
     public Map<String,String> readSymbol(String symbol);
 
     /**
-     * Create a Symbol;
+     * Create a Symbol.
+     * Note that a Stock symbol data is represented as hash table.
+     * 
      * @param symbol
      * @return
      */
     public Map<String,String> createSymbol(String symbol);
 
+    
+    
     /**
-     * Delete a symbol.
+     * Delete a symbol from the database.
      * @param symbol
      */
     public void deleteSymbol(String symbol);
 
+    
+    
     /**
-     * Re-Write to database that you read from;
+     * Overloaded method
+     * Flush or write to database or file that you previously read from;
+     * That's why no arguement is needed.
      */
     public void writeDB();
 
     /**
-     * Write to database 
+     * Overloaded method
+     * Flush or write out data to a database name or a file name.
      * @param dborfile
      */
     public void writeDB(String dborfile);
